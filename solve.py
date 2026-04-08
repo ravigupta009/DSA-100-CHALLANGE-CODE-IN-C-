653742be5dfeb85b5e58c513739660e7a7199df5
Ravi Gupta <ravi720904@gmail.com>
2026-04-08 16:32:25 +0530

    Add files via upload

---
+++ solve.py
@@ -0,0 +1,8 @@
+x = np.linspace(0, 2, 100)
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+ax.plot(x, x, label='Linear')
+ax.plot(x, x**2, label='Quadratic')
+ax.plot(x, x**3, label='Cubic')
+ax.legend()
+ax.set_title('Simple Plot')
+plt.show()
